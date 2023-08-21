419f3fca4f5b823b9858aed5623f20b147bcc5f2
christianEkogha <vekogha@gmail.com>
2023-08-21 15:36:01 +0200

    Update CashCardJsonTest.java

--- src/test/java/fr/cekogha/cashcardapi/CashCardJsonTest.java
+++ src/test/java/fr/cekogha/cashcardapi/CashCardJsonTest.java
@@ -21,34 +21,30 @@ public class CashCardJsonTest {
 
 	@Autowired
 	private JacksonTester<CashCard[]> jsonList;
-	
+
 	private CashCard[] cashCards;
-	
+
 	@BeforeEach
 	void setUp() {
-        cashCards = Arrays.array(
-                new CashCard(99L, 123.45,"christian"),
-                new CashCard(100L, 1.00,"christian"),
-                new CashCard(101L, 150.00,"christian"));
+		cashCards = Arrays.array(new CashCard(99L, 123.45, "christian"), new CashCard(100L, 1.00, "christian"),
+				new CashCard(101L, 150.00, "christian"));
 	}
-	
+
 	@Test
 	public void test() {
 		assertThat(42).isEqualTo(42);
 	}
-	
+
 	@Test
 	public void serializationTest() throws IOException {
 		CashCard card = cashCards[0];
 		assertThat(json.write(card)).isStrictlyEqualToJson("single.json");
 		assertThat(json.write(card)).hasJsonPathNumberValue("@.id");
-		assertThat(json.write(card)).extractingJsonPathNumberValue("@.id")
-		.isEqualTo(99);
+		assertThat(json.write(card)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
 		assertThat(json.write(card)).hasJsonPathNumberValue("@.amount");
-		assertThat(json.write(card)).extractingJsonPathNumberValue("@.amount")
-		.isEqualTo(123.45);
+		assertThat(json.write(card)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
 	}
-	
+
 	@Test
 	public void deserializationTest() throws IOException {
 		String expected = """
@@ -58,14 +54,14 @@ public class CashCardJsonTest {
 					"owner":"christian"
 				}
 				""";
-		assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45,"christian"));
+		assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45, "christian"));
 		assertThat(json.parseObject(expected).id()).isEqualTo(99);
 		assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
 	}
-	
+
 	@Test
 	public void serializationListTest() throws IOException {
-		assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");		
+		assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
 	}
 
 	@Test
